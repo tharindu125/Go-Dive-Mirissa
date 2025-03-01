d4d285190254bfce918d8a30e223e3ac6fe3fc60
Tharindu Ranaweera <tharindu@minthrm.net>
2025-03-01 11:45:08 +0530

    add image slider

--- js/main.js
+++ js/main.js
@@ -104,6 +104,28 @@
             }
         }
     });
+
+    // gallery carousel
+    $(".gallery-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
     
 })(jQuery);
 
